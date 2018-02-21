f6caedd640e28b71658e6e27c81147df5c33c979
eric lima <eric.george.lima@gmail.com>
2018-02-21 16:40:31 -0300

    commit inicial

---
+++ src/main/java/br/edu/cse/EquivalenciaBackApplication.java
@@ -0,0 +1,12 @@
+package br.edu.cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquivalenciaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquivalenciaBackApplication.class, args);
+	}
+}
